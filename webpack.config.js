31374fe7657cedf7da576c1cdabed9e88ffc1b0c
gabrieleinsfeld <gabrieleinsfeld@gmail.com>
2024-07-15 16:29:21 -0300

    Added start instructions and changed webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,18 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
-const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = {
-  entry: "/Users/gabrielsoares/repos/git_template/src/index.js",
+  entry: "/Users/gabrielsoares/repos/dropdown_menu/src/index.js",
   mode: "development",
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "index_bundle.js",
-  },
-  resolve: {
-    fallback: {
-      util: require.resolve("util/"),
-    },
   },
 
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
     }),
-    new NodePolyfillPlugin(),
   ],
   module: {
     rules: [
